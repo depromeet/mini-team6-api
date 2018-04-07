90847a23eb9436468c08d9ff006c3467442f0ece
Codemath <xyrho123@naver.com>
2018-04-07 13:59:51 +0900

    Fix: manytomanyfiled 에 related_name='member'

--- apps/partys/models.py
+++ apps/partys/models.py
@@ -4,7 +4,7 @@ from ..users.models import MyUser
 class Party(models.Model):
     owner = models.ForeignKey(MyUser, on_delete=models.CASCADE,verbose_name="주최자")
     title = models.CharField(max_length=255,verbose_name="회식 제목")
-    member = models.ManyToManyField(MyUser, verbose_name="참여자")
+    member = models.ManyToManyField(MyUser, verbose_name="참여자", related_name='member')
     total = models.IntegerField(verbose_name="전체 금액")
     is_payment = models.BooleanField(default=False, verbose_name="완료 여부")
     create_At = models.DateTimeField(verbose_name="생성 날짜")
