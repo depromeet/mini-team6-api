16bc3eb83c311d13a6a0f14f7047ec3a42ec9b2c
권재원 <springkjw@gmail.com>
2018-04-07 12:30:22 +0900

    소셜 로그인 대비 필드 추가

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,7 +7,10 @@ class MyUserManager(BaseUserManager):
     def _create_user(self, username, password, **kwargs):
         if not username:
             raise ValueError("아이디는 필수입니다.")
-        user = self.model()
+        user = self.model(username=username, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
     def create_user(self, username, password=None, **kwargs):
         kwargs.setdefault('is_admin', False)
@@ -19,19 +22,41 @@ class MyUserManager(BaseUserManager):
 
 
 class MyUser(AbstractBaseUser):
+    PROVIDER = (
+        ('d', '디프만'),
+        ('k', '카카오'),
+    )
+
     # 유저 모델
     username = models.CharField(
         max_length=20,
+        unique=True,
         verbose_name='아이디'
+    )
+    provider = models.CharField(
+        max_length=2,
+        choices=PROVIDER,
+        default='d',
+        verbose_name='가입 경로'
+    )
+    uid = models.CharField(
+        max_length=255,
+        verbose_name="소셜 유저 유니크 아이디"
     )
     name = models.CharField(
         max_length=10,
         verbose_name='이름'
     )
-    email = models.CharField(
+    email = models.EmailField(
         null=True,
         blank=True,
         verbose_name='이메일'
+    )
+    phone = models.CharField(
+        max_length=14,
+        null=True,
+        blank=True,
+        verbose_name='휴대전화'
     )
     is_acitve = models.BooleanField(
         default=True,
@@ -41,10 +66,14 @@ class MyUser(AbstractBaseUser):
         default=False,
         verbose_name='관리자 여부'
     )
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='가입일'
+    )
 
     objects = MyUserManager()
 
-    USERNAME_FIELDS = 'username'
+    USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = [
         'name',
     ]
@@ -56,3 +85,13 @@ class MyUser(AbstractBaseUser):
 
     def __str__(self):
         return self.name
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
