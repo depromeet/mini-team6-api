6956c0cba0c8c26736c7d8e80e65556660871a3b
권재원 <springkjw@gmail.com>
2018-04-07 15:55:41 +0900

    크롤링 데이터 적용

--- apps/prices/management/commands/price.py
+++ apps/prices/management/commands/price.py
@@ -1,6 +1,9 @@
 # 채소 가격 크롤링 하는 커스터 Django 커맨드
 from django.core.management import BaseCommand, CommandError
 
+from apps.prices.crawling import crawling_result
+from apps.prices.models import Price
+
 
 class Command(BaseCommand):
     help = 'Get vegetable price from Crawling'
@@ -12,5 +15,8 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        Price.objects.bulk_create([
+            Price(**data) for data in crawling_result()
+        ])
         self.stdout.write(self.style.SUCCESS(
             'Successfully get vegetable price.'))
--- apps/prices/models.py
+++ apps/prices/models.py
@@ -3,18 +3,16 @@ from django.db import models
 
 class Price(models.Model):
     # 야채 가격 모델
-    NAME = (
-        ('cucumber', '오이'),
-    )
-
     name = models.CharField(
         max_length=50,
-        choices=NAME,
-        default='cucumber',
         verbose_name='품목'
     )
     price = models.SmallIntegerField(
         verbose_name='가격'
+    )
+    unit = models.CharField(
+        max_length=10,
+        verbose_name='단위'
     )
     date = models.DateField(
         verbose_name='기준일'
@@ -23,7 +21,7 @@ class Price(models.Model):
     class Meta:
         db_table = 'price'
         verbose_name = '채소 가격'
-        verbose_name = '채소 가격들'
+        verbose_name_plural = '채소 가격들'
 
     def __str__(self):
         return self.name
